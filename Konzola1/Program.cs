a7b0ef59ec91d4425478983498b141de13c81add
Matko Gečević <mgecevic@racunarstvo.hr>
2023-04-08 15:15:12 +0200

    First testing edition with user authentication

--- Konzola1/Program.cs
+++ Konzola1/Program.cs
@@ -19,5 +19,51 @@ using SoccerDAL.AllRepos.WomenRepos.WomenTeamsResults;
 using SoccerDAL.Comparer;
 
 
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        /*
+         IRepoAllMatches matchesRepo = WomenRepoFactoryAllMatches.GetRepo(); //MenRepoFactoryAllMatches.GetRepo();
+         IList<Matches> matches = await matchesRepo.GetAllMatches();
+         Utility.Print(matches);
+        */
+        /*
+         //Print all matches by country, radi men i women
+         IRepoAllMatchesByCountry matchesRepoByCountry = WomenRepoFactoryMatchesCountry.GetRepo(); //MenRepoFactoryMatchesCountry
+         IList<Matches> matchesCountry = await matchesRepoByCountry.MatchesByCountry("ENG");
+         Utility.Print(matchesCountry);
+        */
 
+        /*
+         //Print all group results, radi men i women
+         IRepoGroupResults repoGroupResults = WomenRepoFactoryGroupResults.GetRepo();
+         IList<Group> groupsResults = await repoGroupResults.GetGroupResults();
+         Utility.Print(groupsResults);
+         */
+        /*
+        IRepoPlayer playerRepository = WomenRepoFactoryPlayer.GetRepo();
+        List<Player> players = playerRepository.GetPlayersFromJsonFile();
+        Utility.Print(players);
+        */
+
+        /* //Print all teams, radi men i women 
+        IRepoTeams teamsRepo = WomenRepoFactoryTeams.GetRepo();
+        IList<Team> teamsList = await teamsRepo.GetAllTeams();
+        Utility.Print(teamsList);
+        */
+
+        /*
+        List<Team> sortedList = new List<Team>(teamsList);
+        sortedList.Sort(new PropertyComparer<Team>("group_id"));
+        Utility.Print(sortedList);
+        */
+
+        /*  //Print all teams results, radi men i women
+        IRepoTeamsResults repoTeamsResults = WomenRepoFactoryTeamsResults.GetRepo();
+        IList<TeamResults> teamResults = await repoTeamsResults.GetTeamsResults();
+        Utility.Print(teamResults);
+        */
+    }
+}
 
