e8a601b68a603c8b1f5ef2b0ec9cddc9b6482555
Matko Gečević <mgecevic@racunarstvo.hr>
2023-04-08 15:13:35 +0200

    First testing edition with user authentication

--- Login/Form1.cs
+++ Login/Form1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualBasic.ApplicationServices;
 using SoccerDAL.Models;
+using System.Drawing.Text;
+using System.Security.Cryptography.X509Certificates;
 using User = SoccerDAL.Models.User;
 
 namespace Login;
@@ -11,10 +13,10 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
-
         IUserRepositoryFactory userRepositoryFactory = new UserFactory();
         IUserFactory userFactory = new UserFactory();
         userManager = new UserManager(userRepositoryFactory, userFactory);
+
     }
 
     private void btnRegister_Click(object sender, EventArgs e)
@@ -22,35 +24,24 @@ public partial class Form1 : Form
         string username = tbUsername.Text;
         string password = tbPassword.Text;
 
-        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-        {
-            MessageBox.Show("Please enter both username and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
-        }
-
-        User newUser = userManager.RegisterUser(username, password);
-
-        if (newUser != null)
-        {
-            MessageBox.Show("User registered successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-        else
-        {
-            MessageBox.Show("User registration failed. The username may already exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
+        NullCheckTextBox(username, password);
+        RegisterUser(username, password);
     }
+
+
 
     private void btnLogin_Click(object sender, EventArgs e)
     {
         string username = tbUsername.Text;
         string password = tbPassword.Text;
 
-        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-        {
-            MessageBox.Show("Please enter both username and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
-        }
+        NullCheckTextBox(username, password);
 
+        UserLogin(username, password);
+    }
+
+    private void UserLogin(string username, string password)
+    {
         User user = userManager.LoginUser(username, password);
 
         if (user != null)
@@ -63,4 +54,27 @@ public partial class Form1 : Form
             MessageBox.Show("Invalid username or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
+
+    private void RegisterUser(string username, string password)
+    {
+        User newUser = userManager.RegisterUser(username, password);
+
+        if (newUser != null)
+        {
+            MessageBox.Show("User registered successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        else
+        {
+            MessageBox.Show("User registration failed. The username may already exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+    private void NullCheckTextBox(string username, string password)
+    {
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            MessageBox.Show("Please enter both username and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+    }
+
 }
